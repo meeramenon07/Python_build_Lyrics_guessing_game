1189a21fb764ca40fa961becb9fc2e2227d9ab3f
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2023-09-14 16:09:20 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Let us start the game for name the song lyric challenge!")
+print()
+print("Enter the missing lyric in the song and become cool like me!")
+counter = 1
+while True:
+  lyric = input("Say my ___")
+  if lyric == "name" or lyric == "Name":
+  print("You got it right!")
+ else:
+   print("No. Try again!")
+   count += 1
+if lyric == "name" or lyric == "Name":
+  break
+print("That was a good game, thanks for playing")
+print("You got right answer,", counter,"attempts")
+  
